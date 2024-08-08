0d762245e6af62ac3946df651a3e5d2fdf2d6e28
alvaro <alvarodevcode@outlook.com>
2024-08-07 23:25:53 -0500

    feat: :adhesive_bandage: adds hook and impro to components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-ring focus-visible:ring-4 focus-visible:ring-ring/30 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50",
+          "flex h-[36px] w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-ring focus-visible:ring-4 focus-visible:ring-ring/30 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-50",
           className,
         )}
         ref={ref}
