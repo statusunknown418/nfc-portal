c2449743d6e939b6b6a9c35f756f205a687dde21
alvaro <alvarodevcode@outlook.com>
2024-09-02 09:57:46 -0500

    style: updates

--- src/components/admin/contact/contact-data/contact-data-wrapper.tsx
+++ src/components/admin/contact/contact-data/contact-data-wrapper.tsx
@@ -2,6 +2,7 @@ import { api } from "~/trpc/server";
 import { ContactDataForm } from "./ContactDataForm";
 import { auth } from "~/server/auth";
 import { ToggleContactVisibility } from "../visibility/ToggleContactVisibility";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export const ContactDataWrapper = async () => {
   const [data, session] = await Promise.all([api.vCard.get(), auth()]);
@@ -15,5 +16,46 @@ export const ContactDataWrapper = async () => {
 };
 
 export const ContactDataLoading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="grid w-full grid-cols-1 gap-4">
+      <Skeleton className="h-14 w-full" />
+
+      <section className="grid grid-cols-1 gap-4 rounded-xl border p-4">
+        <div className="mt-2 flex gap-2">
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 flex-grow" />
+        </div>
+
+        <div className="flex gap-2">
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 flex-grow" />
+        </div>
+
+        <div className="flex gap-2">
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 w-40" />
+          <Skeleton className="h-10 w-40" />
+        </div>
+
+        <div className="flex gap-2">
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 flex-grow" />
+        </div>
+
+        <div className="flex gap-2">
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 w-40" />
+          <Skeleton className="h-10 w-40" />
+        </div>
+
+        <div className="flex gap-2">
+          <Skeleton className="h-10 flex-grow" />
+          <Skeleton className="h-10 w-40" />
+          <Skeleton className="h-10 w-40" />
+        </div>
+      </section>
+    </div>
+  );
 };
