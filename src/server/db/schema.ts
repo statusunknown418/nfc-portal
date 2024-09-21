8ef7890d28f4e671942587a00bb1cde5cbb042c4
alvaro <alvarodevcode@outlook.com>
2024-09-21 00:18:38 -0500

    fix: db & package updates should fix builds and stuff

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -190,7 +190,10 @@ export const users = sqliteTable("user", {
   ),
   cardVariant: text("card_variant", { enum: cardVariants }).notNull().default("basic"),
   avatarShape: text("avatar_shape", { enum: avatarShapes }).notNull().default("rounded"),
-  theme: text("theme", { mode: "json" }).notNull().$type<ThemeType>().default(BASE_THEMES.default),
+  theme: text("theme", { mode: "json" })
+    .notNull()
+    .$type<ThemeType>()
+    .$defaultFn(() => BASE_THEMES.default),
 });
 
 export const userProfileSchema = createInsertSchema(users, {
