2e5b48f7d6a5bfa4624dae47dbc6c436f77b575f
alvaro <alvarodevcode@outlook.com>
2024-09-23 20:03:47 -0500

    fix: build fix

--- src/components/magicui/grid-pattern.tsx
+++ src/components/magicui/grid-pattern.tsx
@@ -3,14 +3,14 @@ import { useId } from "react";
 import { cn } from "~/lib/utils";
 
 interface GridPatternProps {
-  width?: any;
-  height?: any;
-  x?: any;
-  y?: any;
+  width?: number;
+  height?: number;
+  x?: number;
+  y?: number;
   squares?: Array<[x: number, y: number]>;
-  strokeDasharray?: any;
+  strokeDasharray?: number;
   className?: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }
 
 export function GridPattern({
@@ -35,19 +35,8 @@ export function GridPattern({
       {...props}
     >
       <defs>
-        <pattern
-          id={id}
-          width={width}
-          height={height}
-          patternUnits="userSpaceOnUse"
-          x={x}
-          y={y}
-        >
-          <path
-            d={`M.5 ${height}V.5H${width}`}
-            fill="none"
-            strokeDasharray={strokeDasharray}
-          />
+        <pattern id={id} width={width} height={height} patternUnits="userSpaceOnUse" x={x} y={y}>
+          <path d={`M.5 ${height}V.5H${width}`} fill="none" strokeDasharray={strokeDasharray} />
         </pattern>
       </defs>
       <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
