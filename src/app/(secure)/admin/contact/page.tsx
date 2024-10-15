a447494985deb9cfa18b4614cec2c2780e1ce861
alvaro <alvarodevcode@outlook.com>
2024-10-14 22:03:58 -0500

    feat: adding better stuff
    just gotta build the iOS contact UI preview and we're good to go

--- src/app/(secure)/admin/contact/page.tsx
+++ src/app/(secure)/admin/contact/page.tsx
@@ -1,4 +1,10 @@
-import { Metadata } from "next";
+import { type Metadata } from "next";
+import { Suspense } from "react";
+import {
+  ContactDataLoading,
+  ContactDataWrapper,
+} from "~/components/admin/contact/contact-data/contact-data-wrapper";
+import { ContactPreviewWrapper } from "~/components/admin/contact/preview/contact-preview-wrapper";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,27 +14,37 @@ import {
 } from "~/components/ui/breadcrumb";
 
 export const metadata: Metadata = {
-  title: "Información de contact",
+  title: "Información de contacto",
 };
 
 export default function ContactPage() {
   return (
-    <section className="grid h-full w-full grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-10">
-      <section className="flex flex-col gap-4">
-        <Breadcrumb className="self-start">
-          <BreadcrumbList>
-            <BreadcrumbItem>Dashboard</BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbPage>Card customization</BreadcrumbPage>
-          </BreadcrumbList>
-        </Breadcrumb>
+    <section className="flex flex-col gap-4">
+      <Breadcrumb className="self-start">
+        <BreadcrumbList>
+          <BreadcrumbItem>Dashboard</BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbPage>Contacto</BreadcrumbPage>
+        </BreadcrumbList>
+      </Breadcrumb>
 
-        <header>
-          <h1 className="text-2xl font-bold">Contact info</h1>
-          <p className="mt-1 text-sm text-muted-foreground">
-            Decide what information you want to share with your contacts.
-          </p>
-        </header>
+      <section className="grid h-full w-full grid-cols-1 gap-8 lg:grid-cols-2">
+        <article className="flex flex-col gap-4">
+          <header>
+            <h1 className="text-2xl font-bold">Información de contacto</h1>
+            <p className="mt-1 text-sm text-muted-foreground">
+              Decide qué información personal quieres compartir con tus contactos.
+            </p>
+          </header>
+
+          <Suspense fallback={<ContactDataLoading />}>
+            <ContactDataWrapper />
+          </Suspense>
+        </article>
+
+        <Suspense>
+          <ContactPreviewWrapper />
+        </Suspense>
       </section>
     </section>
   );
